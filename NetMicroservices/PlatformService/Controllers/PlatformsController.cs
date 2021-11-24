aa8419a664fd8305a8d4a2e0dc26a3af8d75c001
Nick Gaudreau <nickgaudreau@outlook.com>
2021-11-24 10:31:40 -0500

    platform ctrl, get and get by id endpoints + mappping and dto

---
+++ NetMicroservices/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,11 @@
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
