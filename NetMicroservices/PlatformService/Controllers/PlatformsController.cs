8ad84e47b8ccec09253bc60089544bd22858a49f
Nick Gaudreau <nickgaudreau@outlook.com>
2021-11-26 10:47:13 -0500

    platform service ctrl. get all, by id and create platform, dto, mapping

--- NetMicroservices/PlatformService/Controllers/PlatformsController.cs
+++ NetMicroservices/PlatformService/Controllers/PlatformsController.cs
@@ -1,3 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(
+            IPlatformRepo repository,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms....");
+
+            var platformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+
+            return NotFound();
+        }
+
         [HttpPost]
         public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
         {
@@ -9,3 +56,5 @@
 
             return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
         }
+    }
+}
--- NetMicroservices/PlatformService/Dtos/PlatformCreateDto.cs
+++ NetMicroservices/PlatformService/Dtos/PlatformCreateDto.cs
@@ -1,23 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace PlatformService.Models
+namespace PlatformService.Dtos
 {
-    public class Platform
+    public class PlatformCreateDto
     {
-        [Key]
         [Required]
-        public int Id { get; set; }
-
-        [Required] // Cannot be null
-        [MaxLength(128)]
         public string Name { get; set; }
 
         [Required]
-        [MaxLength(256)]
         public string Publisher { get; set; }
 
         [Required]
-        [MaxLength(64)]
         public string Cost { get; set; }
     }
 }
