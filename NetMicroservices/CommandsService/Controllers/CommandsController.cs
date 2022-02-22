3e03f9187ff5622d9d51bc7e5f8482cad61d9085
Nick Gaudreau <nickgaudreau@outlook.com>
2022-02-21 19:44:49 -0500

    command contrl with cRUD endpoints

---
+++ NetMicroservices/CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId}");
+
+            if (!_repository.PlaformExits(platformId))
+            {
+                return null; // 204
+                // return NotFound(); // 404 with body
+            }
+
+            var commands = _repository.GetCommandsForPlatform(platformId);
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+        {
+            Console.WriteLine($"--> Hit GetCommandForPlatform: {platformId} / {commandId}");
+
+            if (!_repository.PlaformExits(platformId))
+            {
+                return null; // 204
+                // return NotFound(); // 404 with body
+            }
+
+            var command = _repository.GetCommand(platformId, commandId);
+
+            if(command == null)
+            {
+                return null; // 204
+                // return NotFound(); // 404 with body
+            }
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommandForPlatform(int platformId, CommandCreateDto commandDto)
+        {
+             Console.WriteLine($"--> Hit CreateCommandForPlatform: {platformId}");
+
+            if (!_repository.PlaformExits(platformId))
+            {
+                return null; // 204
+                // return NotFound(); // 404 with body
+            }
+
+            var command = _mapper.Map<Command>(commandDto);
+
+            _repository.CreateCommand(platformId, command);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(command);
+
+            return CreatedAtRoute(nameof(GetCommandForPlatform),
+                new {platformId = platformId, commandId = commandReadDto.Id}, commandReadDto);
+        }
+
+    }
+}
