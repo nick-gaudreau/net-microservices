6fc050f65847741444f5cdc35bcd47c5d22946a0
Nick Gaudreau <nickgaudreau@outlook.com>
2022-03-09 09:10:30 -0500

    publish to queue on create

--- NetMicroservices/PlatformService/Controllers/PlatformsController.cs
+++ NetMicroservices/PlatformService/Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
@@ -17,14 +18,17 @@ namespace PlatformService.Controllers
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
         private readonly ICommandDataClient _commandDataClient;
+        private readonly IMessageBusClient _messageBusClient;
 
         public PlatformsController(
             IPlatformRepo repository,
-            IMapper mapper, ICommandDataClient commandDataClient)
+            IMapper mapper, ICommandDataClient commandDataClient, IMessageBusClient messageBusClient)
         {
             _repository = repository;
             _mapper = mapper;
             _commandDataClient = commandDataClient;
+            _messageBusClient = messageBusClient;
+            _messageBusClient = messageBusClient;
         }
 
         [HttpGet]
@@ -66,6 +70,18 @@ namespace PlatformService.Controllers
             catch (Exception ex)
             {
                 Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
+            }
+
+            //Send Async Message
+            try
+            {
+                var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
+                platformPublishedDto.Event = "Platform_Published";
+                _messageBusClient.PublishNewPlatform(platformPublishedDto);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not send asynchronously: {ex.Message}");
             }
 
             return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
---
+++ NetMicroservices/PlatformService/Dtos/PlatformPublishedDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PlatformService.Dtos
+{
+    /// <summary>
+    /// Message Bus publish DTO
+    /// </summary>
+    public class PlatformPublishedDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Event { get; set; }
+    }
+}
