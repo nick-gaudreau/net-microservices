2aff662c76eaa5df68088a640775d8ea11c56183
Nick Gaudreau <nickgaudreau@outlook.com>
2022-03-09 09:09:47 -0500

    RabbitMQ message bus client to publish

---
+++ NetMicroservices/PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient, IDisposable
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        // IConfiguration always avaialble for DI
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+            try
+            {
+                _connection = factory.CreateConnection();
+               
+                _channel = _connection.CreateModel();
+
+                _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                // Event callback += OurCustomMethod(ConnectionShutdownExpectedParam)
+                _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+                // Event callback += OurCustomMethod(CallbackExceptionExpectedParam)
+                _connection.CallbackException += RabbitMQ_Exception;
+
+                Console.WriteLine("--> Connected to MessageBus");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not connect to the Message Bus: {ex.Message}");
+            }
+        }
+
+        public void PublishNewPlatform(PlatformPublishedDto platformPublishedDto)
+        {
+            var message = JsonSerializer.Serialize(platformPublishedDto);
+
+            if (_connection.IsOpen)
+            {
+                Console.WriteLine("--> RabbitMQ Connection Open, sending message...");
+                SendMessage(message);
+            }
+            else
+            {
+                Console.WriteLine("--> RabbitMQ connectionis closed, not sending");
+            }   
+        }
+
+        private void SendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(exchange: "trigger",
+                            routingKey: "",
+                            basicProperties: null,
+                            body: body);
+            Console.WriteLine($"--> We have sent {message}");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("MessageBus Disposed");
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("--> RabbitMQ Connection Shutdown");
+        }
+
+        private void RabbitMQ_Exception(object sender, CallbackExceptionEventArgs e)
+        {
+            Console.WriteLine($"--> RabbitMQ Exception: {e?.Exception?.Message}");
+        }
+    }
+}
